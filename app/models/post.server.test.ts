8e936771b4f3cc78abadb76bba35deca27f22c45
Masayuki Onishi <47713499+masayuki-0319@users.noreply.github.com>
2024-04-04 00:54:43 +0900

    Revert "fishely から fabbrica に置換する"
    This reverts commit e5b00949f301653a361e1094ccb229e431e36b56.

--- app/models/post.server.test.ts
+++ app/models/post.server.test.ts
@@ -1,11 +1,11 @@
-import { PostTestFactory } from '~/test/factories/post';
+import { postTestFactory } from '~/test/factories/post';
 
 import { postRepository } from './post.server';
 
 describe('PostRepository', () => {
   describe('create', () => {
     it('新しいポストを登録できる', async () => {
-      const params = await PostTestFactory.build();
+      const params = postTestFactory.build();
 
       const createdPost = await postRepository.create(params);
 
@@ -16,7 +16,7 @@ describe('PostRepository', () => {
 
   describe('find', () => {
     it('引数の id が存在すると、ポストを取得できる', async () => {
-      const post = await PostTestFactory.create();
+      const post = await postTestFactory.create();
       const foundPost = await postRepository.find({ id: post.id });
 
       expect(foundPost).toBeDefined();
@@ -26,7 +26,7 @@ describe('PostRepository', () => {
 
   describe('findAll', () => {
     it('全てのポストを取得できる', async () => {
-      await PostTestFactory.createList(3);
+      await postTestFactory.createList(3);
 
       const posts = await postRepository.findAll();
 
@@ -36,7 +36,7 @@ describe('PostRepository', () => {
 
   describe('delete', () => {
     it('引数の id が存在すると、ポストを削除できる', async () => {
-      const post = await PostTestFactory.create();
+      const post = await postTestFactory.create();
 
       await postRepository.delete({ id: post.id });
 
--- app/test/factories/post.ts
+++ app/test/factories/post.ts
@@ -1,8 +1,17 @@
-import { definePostFactory } from '../__generated__/fabbrica';
+import { Post } from '@prisma/client';
+import { Factory } from 'fishery';
 
-export const PostTestFactory = definePostFactory({
-  defaultData: async ({ seq }) => ({
-    title: `test-${seq}`,
-    content: `test-${seq}`,
-  }),
-});
+import { postRepository } from '~/models/post.server';
+
+export const postTestFactory = Factory.define<Post>(
+  ({ sequence, onCreate }) => {
+    onCreate((post) => postRepository.create(post));
+
+    return {
+      id: `uuid-${sequence}`,
+      title: `test-${sequence}`,
+      content: `test-${sequence}`,
+      published: true,
+    };
+  }
+);
